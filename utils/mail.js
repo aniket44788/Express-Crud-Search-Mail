045e4bdb31046e08afaeaeb8c5ba048506a72774
aniket44788 <darkpanda44788@gmail.com>
2025-03-01 12:25:33 +0530

    update

---
+++ utils/mail.js
@@ -0,0 +1,12 @@
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, 
+  auth: {
+    user: process.env.NODE_EMAIL,
+    pass: process.env.NODE_PASS,
+  },
+});
+ module.exports = transporter
